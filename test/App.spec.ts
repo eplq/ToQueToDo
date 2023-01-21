73d61037e05e06164988ab98488e9a2bb84f7eab
eplq <91148468+eplq@users.noreply.github.com>
2023-01-21 23:58:25 +0100

    test

--- test/App.spec.ts
+++ test/App.spec.ts
@@ -7,6 +7,7 @@ describe("Test", () => {
     afterEach(() => cleanup())
 
     it("it renders", () => {
+        // @ts-expect-error i dont know why ts cant detect Svelte component type
         render(App)
 
         const title = screen.getByText("preparado")
