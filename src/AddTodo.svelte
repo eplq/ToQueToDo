b17f9904f0d59e791fd3df67fc6c39338d317743
eplq <91148468+eplq@users.noreply.github.com>
2023-01-22 19:09:55 +0100

    add todo

---
+++ src/AddTodo.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { tasks } from "./taskStore";
+
+    let task = "";
+
+    function addTask() {
+        if (task)
+            tasks.update(($taskList) => [
+                ...$taskList,
+                {
+                    task: task,
+                    done: false,
+                },
+            ]);
+    }
+</script>
+
+<div class="mt-8 flex">
+    <input
+        type="text"
+        placeholder="¿Qué tenemos que hacer?"
+        class="w-auto flex-grow bg-transparent border border-neutral-200 rounded pl-2 text-neutral-200 focus:border-green-700 outline-none overflow-auto"
+        bind:value={task}
+    />
+    <button
+        class="ml-4 text-lg border border-neutral-200 rounded px-4 py-1 text-neutral-200 hover:from-red-500 hover:to-orange-500 hover:bg-gradient-to-tr hover:border-transparent active:opacity-75" on:click={addTask}
+        >Save</button
+    >
+</div>
--- src/App.svelte
+++ src/App.svelte
@@ -5,5 +5,6 @@
 <div class="h-screen bg-gradient-to-tr from-neutral-900 to-orange-900 flex flex-col items-center py-5">
     <div class="border-neutral-200 border-2 rounded w-11/12 md:w-4/6 lg:w-2/6 p-3">
         <h1 class="text-center font-bold text-5xl mt-4 bg-gradient-to-r bg-clip-text text-transparent from-neutral-500 to-neutral-300">To Qué To Do</h1>
+        <AddTodo />
     </div>
 </div>
